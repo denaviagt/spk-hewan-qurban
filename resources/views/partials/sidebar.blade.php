e21be249b2fa8baf313b03dda201b5fb04980b97
Anggita Denavia <anggitadenav@gmail.com>
2022-07-09 12:51:24 +0700

    Templating master

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,36 @@
+   <!-- partial:partials/_sidebar.html -->
+   <nav class="sidebar sidebar-offcanvas" id="sidebar">
+       <ul class="nav">
+           <li class="nav-item">
+               <a class="nav-link" href="{{ url('dashboard') }}">
+                   <i class="icon-grid menu-icon"></i>
+                   <span class="menu-title">Dashboard</span>
+               </a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="{{ url('kriteria') }}">
+                   <i class="icon-layout menu-icon"></i>
+                   <span class="menu-title">Kriteria</span>
+               </a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="{{ url('hewan') }}">
+                   <i class="mdi mdi-cow menu-icon"></i>
+                   <span class="menu-title">Daftar Hewan</span>
+               </a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="#">
+                   <i class="icon-columns menu-icon"></i>
+                   <span class="menu-title">Penilaian</span>
+               </a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="#">
+                   <i class="icon-paper menu-icon"></i>
+                   <span class="menu-title">Hasil Analisa</span>
+               </a>
+           </li>
+       </ul>
+   </nav>
+   <!-- partial -->
